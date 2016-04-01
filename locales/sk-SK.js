f5ebf3ae0523d7712ab6997ba31883bd93b49490
gasstan <gasstan@gmail.com>
2016-04-01 13:55:32 +0200

    added Slovak language

---
+++ locales/sk-SK.js
@@ -0,0 +1,97 @@
+var commands = {
+    "list": {
+        "text": "Čo môžem povedať?",
+        "voice": "Čo môžem povedať",
+        "description": "Zobrazí list príkazov ktoré sú dostupné."
+    },
+    "home": {
+        "text": "Domov",
+        "voice": "Domov",
+        "description": "Návrat na domovskú obrazovku"
+    },
+    "sleep": {
+        "text": "Spi",
+        "voice": "Spi",
+        "description": "Uspí zrkadlo"
+    },
+    "wake_up": {
+        "text": "Vstávaj",
+        "voice": "Vstávaj",
+        "description": "Prebudí zrkadlo"
+    },
+    "debug": {
+        "text": "Show debug information",
+        "voice": "Show debug information",
+        "description": "Show debug information"
+    },
+    "map_show": {
+        "text": "Zobraz mapu",
+        "voice": "Zobraz mapu",
+        "description": "Zobrazí mapu tvojho aktuálneho mesta."
+    },
+    "map_location": {
+        "text": "Ukáž mapu ________",
+        "voice": "Zobraz mi mapu *location",
+        "description": "Zobrazí mapu zvolenej lokality."
+    },
+    "map_zoom_in": {
+        "text": "Priblíž",
+        "voice": "(map) priblíž",
+        "description": "Priblíži aktuálne zobrazenú mapu"
+    },
+    "map_zoom_out": {
+        "text": "Oddiaľ",
+        "voice": "(map) oddiaľ",
+        "description": "Oddiali aktuálne zobrazenú mapu"
+    },
+    "map_zoom_point": {
+        "text": "Priblíž na ________",
+        "voice": "(map) priblíž (na) *value",
+        "description": "Priblíži na indikovaný bod"
+    },
+    "map_zoom_reset": {
+        "text": "Resetni zoom",
+        "voice": "(map) resetni zoom",
+        "description": "Resetuje zoom"
+    },
+    "account_set_name": {
+        "text": "Volám sa ________",
+        "voice": "Volám sa *name",
+        "description": "Volám sa *name"
+    },
+    "images_search": {
+        "text": "Ukaž mi ________",
+        "voice": "Ukaž mi *term",
+        "description": "Zobrazí obrázky požadovanej veci."
+    },
+    "image_giphy": {
+        "text": "Giphy ________",
+        "voice": "giphy *img",
+        "description": "Zobrazí giphy obrázok"
+    },
+    "image_comic": {
+        "text": "Ukaž xkcd",
+        "voice": "Ukaž xkcd",
+        "description": "Zobrazí najnovší xkcd komiks."
+    },
+    "reminder_insert": {
+        "text": "Pripomeň mi ________",
+        "voice": "Pripomeň mi *task",
+        "description": "Pridá dačo do listu pripomienok."
+    },
+    "reminder_clear": {
+        "text": "Vyčisti pripomienky",
+        "voice": "Vyčisti pripomienky",
+        "description": "Vyčistí list pripomienok."
+    },
+    "time_show": {
+        "text": "Koľko je hodín?",
+        "voice": "Koľko je hodín",
+        "description": "Koľko je hodín"
+    },
+    "light_action": {
+        "text": "Turn the (state) light (action)",
+        "voice": "(turn) (the) :state (the) light(s) *action",
+        "description": "(turn) (the) :state (the) light(s) *action"
+    }
+};
---
+++ locales/sk.js
@@ -0,0 +1,1 @@
+document.write('\x3Cscript src="locales/sk-SK.js">\x3C/script>');
