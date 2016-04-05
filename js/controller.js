0f938440adc0a81f65892ca7cb0844d6b7d8738d
gasstan <gasstan@gmail.com>
2016-04-05 22:14:08 +0200

    div for RFID

--- js/controller.js
+++ js/controller.js
@@ -290,7 +290,7 @@
         .controller('Theme', themeController);
 
     function rfidController($scope) {
-        $scope.read = false;
+        $scope.read = true;
     }
 
     angular.module('SmartMirror')
